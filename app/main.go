ded2f666d7a4af2d54d6130918bd39760b9f904a
phu <baphu95@gmail.com>
2020-07-12 23:22:50 +0700

    2

--- app/main.go
+++ app/main.go
@@ -14,34 +14,36 @@ type User struct {
 	Name string
 	Age  int64
 }
-
+var db *gorm.DB
+var err error
 func main() {
+	db, err = gorm.Open("mysql", "docker:docker@tcp(db:3306)/test_db?parseTime=true")
+	if err != nil {
+		panic("failed to connect database")
+	}
 	createTable()
 	r := gin.Default()
-	r.GET("/ping/:id", func(c *gin.Context) {
-		db, err := gorm.Open("mysql", "docker:docker@tcp(db:3306)/test_db?parseTime=true")
-		if err != nil {
-				log.Fatal(err)
-		}
-		id := c.Param("id")
-		var user User
-		  db.First(&user, id)
-          log.Println(user)
-          log.Println(user.Name)
-          log.Println(user.Age)
-            fmt.Println("asaa")
-		c.JSON(200, gin.H{"name": user.Name, "age": user.Age})
+	users := r.Group("/users")
+	users.GET("/:id", getUser)
+	users.POST("/edit", func(c *gin.Context) {
+		c.JSON(200, gin.H{"name": "user.Name", "age": "user.Age"})
 	})
-
 	r.Run()
 }
 
 func createTable() {
-	db, err := gorm.Open("mysql", "docker:docker@tcp(db:3306)/test_db")
-	if err != nil {
-	  panic("failed to connect database")
-	}
-    db.Exec("TRUNCATE TABLE users;")
+	db.Exec("TRUNCATE TABLE users;")
 	db.AutoMigrate(&User{})
 	db.Create(&User{Name: "phule", Age: 25})
+}
+
+func getUser(c *gin.Context) {
+	id := c.Param("id")
+	var user User
+	db.First(&user, id)
+	log.Println(user)
+	log.Println(user.Name)
+	log.Println(user.Age)
+	fmt.Println("asaa1")
+	c.JSON(200, gin.H{"name": user.Name, "age": user.Age})
 }
